c7b64be23c6069f1ed64fcde38d5eeab74f617bf
Ojasv Dixit <115476649+Ojasvdixit@users.noreply.github.com>
2025-09-07 13:12:02 +0530

    App router : add Forgot Password and Login pages with initial structure

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+//! In Next.js (App Router), a Route Group is a way to organize your routes into folders without affecting the URL path. 
+
+//@ 👉 Basically, it’s a folder inside the app/ directory wrapped in parentheses ( ), and Next.js ignores the folder name when building routes.
+
+export default function LoginPage() { 
+  return <div>Login Page</div>;
+}
---
+++ src/app/(auth)/register/register.tsx
@@ -0,0 +1,3 @@
+export default function RedgisterPage() { 
+  return <div>Register Page</div>;
+}
