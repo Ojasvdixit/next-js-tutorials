2df3929e24bdff4c35dea97784ef4641e9cec797
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2026-03-06 10:08:20 +0530

    dev-eventsproject set up and home page frontend

---
+++ dev-event-project/lib/contants.ts
@@ -0,0 +1,18 @@
+export const EVENTS = [
+  {
+    image: "/images/event1.png",
+    title: "Event 1",
+    slug: "event-1",
+    location: "location-1",
+    date: "date-1",
+    time: "time-1",
+  },
+  {
+    image: "/images/event2.png",
+    title: "Event 2",
+    slug: "event-2",
+    location: "location-2",
+    date: "date-2",
+    time: "time-2",
+  },
+];
